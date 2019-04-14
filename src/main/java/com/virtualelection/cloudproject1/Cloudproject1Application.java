73a95f5dc939e682dcadf3c0ac0e4856b5e170fb
Sajjad75 <45334897+Sajjad75@users.noreply.github.com>
2019-04-14 12:56:50 +0430

    Add files via upload

---
+++ src/main/java/com/virtualelection/cloudproject1/Cloudproject1Application.java
@@ -0,0 +1,13 @@
+package com.virtualelection.cloudproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cloudproject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cloudproject1Application.class, args);
+    }
+
+}
